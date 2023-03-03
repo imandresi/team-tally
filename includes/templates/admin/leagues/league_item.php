c1c212240c40a95cdf006fc46d7181372093010a
imandresi <contact@mandresi.net>
2023-03-03 12:00:28 +0300

    feat: make each league's logo clickable

--- includes/templates/admin/leagues/league_item.php
+++ includes/templates/admin/leagues/league_item.php
@@ -10,8 +10,10 @@
 
 <div class="teamtally_leagues__league-item league-item league-item-id-{{league_id}}">
     <div class="thumbnail">
-        <div class="thumbnail__content" style="background-image: url('{{{league_photo_url}}}')"></div>
-        <div class="thumbnail__legend"><a href="{{{manage_teams_url}}}">Click to manage TEAMS</a></div>
+        <a href="{{{manage_teams_url}}}">
+            <span class="thumbnail__content" style="background-image: url('{{{league_photo_url}}}')"></span>
+            <span class="thumbnail__legend">Click to manage TEAMS</span>
+        </a>
     </div>
     <div class="footer">
         <div class="league-data">
@@ -21,9 +23,9 @@
         <div class="league-item-buttons">
             <a href="{{{edit_league_url}}}" class="teamtally_btn teamtally_icon teamtally_icon_edit"></a>
             <div
-               class="teamtally_btn teamtally_icon teamtally_icon_delete teamtally-delete teamtally_pointer_cursor"
-               data-league-id="{{league_id}}"
-               data-remove-url="{{{remove_league_url}}}"></div>
+                    class="teamtally_btn teamtally_icon teamtally_icon_delete teamtally-delete teamtally_pointer_cursor"
+                    data-league-id="{{league_id}}"
+                    data-remove-url="{{{remove_league_url}}}"></div>
         </div>
     </div>
 </div>
